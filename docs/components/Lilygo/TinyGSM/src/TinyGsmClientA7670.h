a0e3eec9e936c522cabcd256f9d6435e34a7550d
trondve <82293452+trondve@users.noreply.github.com>
2026-03-07 21:25:17 +0100

    SIM7000G documentation

---
+++ docs/components/Lilygo/TinyGSM/src/TinyGsmClientA7670.h
@@ -0,0 +1,568 @@
+/**
+ * @file      TinyGsmClientA7670.h
+ * @author    Lewis He (lewishe@outlook.com)
+ * @license   MIT
+ * @copyright Copyright (c) 2023  Shenzhen Xin Yuan Electronic Technology Co., Ltd
+ * @date      2024-12-05
+ *
+ */
+
+#ifndef SRC_TINYGSMCLIENTA7670_H_
+#define SRC_TINYGSMCLIENTA7670_H_
+
+// #define TINY_GSM_DEBUG Serial
+// #define TINY_GSM_USE_HEX
+
+#define TINY_GSM_MUX_COUNT 10
+#define TINY_GSM_BUFFER_READ_AND_CHECK_SIZE
+
+#include "TinyGsmClientA76xx.h"
+#include "TinyGsmMqttA76xx.h"
+#include "TinyGsmHttpsComm.h"
+#include "TinyGsmTCP.tpp"
+#include "TinyGsmFSComm.tpp"
+
+/*
+* UC6228CI lite G1B1E1 COM1
+* PN N/A
+* HWVer N/A
+* FWVer R3.4.21.0Build16211
+* Copyright (c), Unicore Communications Inc.
+* All rights reserved.
+* */
+enum A7670X_GPSMode {
+  GNSS_MODE_GPS_BDS_QZSS = 1,               //  GPS L1+SBAS+QZSS
+  GNSS_MODE_BDS = 2,                        //  BDS B1
+  GNSS_MODE_GPS_QZSS = 3,                   //  GPS+GLONASS+GALILEO+SBAS+QZSS
+  GNSS_MODE_GPS_BDS_GALILEO_SBAS_QZSS = 4   //  GPS+BDS+GALILEO+SBAS+QZSS
+};
+
+class TinyGsmA7670 :  public TinyGsmA76xx<TinyGsmA7670>,
+                      public TinyGsmTCP<TinyGsmA7670, TINY_GSM_MUX_COUNT>,
+                      public TinyGsmMqttA76xx<TinyGsmA7670, TINY_GSM_MQTT_CLI_COUNT>,
+                      public TinyGsmHttpsComm<TinyGsmA7670,ASR_A7670X>, 
+                      public TinyGsmFSComm<TinyGsmA7670,ASR_A7670X>{
+  friend class TinyGsmA76xx<TinyGsmA7670>;
+  friend class TinyGsmTCP<TinyGsmA7670, TINY_GSM_MUX_COUNT>;
+  friend class TinyGsmMqttA76xx<TinyGsmA7670, TINY_GSM_MQTT_CLI_COUNT>;
+  friend class TinyGsmHttpsComm<TinyGsmA7670,ASR_A7670X>;
+  friend class TinyGsmFSComm<TinyGsmA7670,ASR_A7670X>;
+
+  /*
+   * Inner Client
+   */
+ public:
+  class GsmClientA7670 : public GsmClient {
+    friend class TinyGsmA7670;
+
+   public:
+    GsmClientA7670() {}
+
+    explicit GsmClientA7670(TinyGsmA7670& modem, uint8_t mux = 0) {
+      init(&modem, mux);
+    }
+
+    bool init(TinyGsmA7670* modem, uint8_t mux = 0) {
+      this->at       = modem;
+      sock_available = 0;
+      prev_check     = 0;
+      sock_connected = false;
+      got_data       = false;
+
+      if (mux < TINY_GSM_MUX_COUNT) {
+        this->mux = mux;
+      } else {
+        this->mux = (mux % TINY_GSM_MUX_COUNT);
+      }
+      at->sockets[this->mux] = this;
+
+      return true;
+    }
+
+   public:
+    virtual int connect(const char* host, uint16_t port, int timeout_s) {
+      // stop();
+      TINY_GSM_YIELD();
+      rx.clear();
+      sock_connected = at->modemConnect(host, port, mux, false, timeout_s);
+      return sock_connected;
+    }
+    TINY_GSM_CLIENT_CONNECT_OVERRIDES
+
+    void stop(uint32_t maxWaitMs) {
+      dumpModemBuffer(maxWaitMs);
+      at->sendAT(GF("+CIPCLOSE="), mux);
+      sock_connected = false;
+      at->waitResponse();
+    }
+    void stop() override {
+      stop(15000L);
+    }
+
+    /*
+     * Extended API
+     */
+
+    String remoteIP() TINY_GSM_ATTR_NOT_IMPLEMENTED;
+  };
+
+  /*
+   * Inner Secure Client
+   */
+  // NOTE:  Use modem TINYGSMA7670SSL for a secure client!
+
+  /*
+   * Constructor
+   */
+ public:
+  explicit TinyGsmA7670(Stream& stream)
+      : TinyGsmA76xx<TinyGsmA7670>(stream) {
+    memset(sockets, 0, sizeof(sockets));
+  }
+
+  /*
+   * Basic functions
+   */
+ protected:
+  bool initImpl(const char* pin = NULL) {
+    DBG(GF("### TinyGSM Version:"), TINYGSM_VERSION);
+    DBG(GF("### TinyGSM Compiled Module:  TinyGsmClientA7670"));
+
+    if (!testAT()) { return false; }
+
+    sendAT(GF("E0"));  // Echo Off
+    if (waitResponse() != 1) { return false; }
+
+#ifdef TINY_GSM_DEBUG
+    sendAT(GF("+CMEE=2"));  // turn on verbose error codes
+#else
+    sendAT(GF("+CMEE=0"));  // turn off error codes
+#endif
+    waitResponse();
+
+    DBG(GF("### Modem:"), getModemName());
+
+    // Disable time and time zone URC's
+    sendAT(GF("+CTZR=0"));
+    if (waitResponse(10000L) != 1) { return false; }
+
+    // Enable automatic time zome update
+    sendAT(GF("+CTZU=1"));
+    if (waitResponse(10000L) != 1) { return false; }
+
+    SimStatus ret = getSimStatus();
+    // if the sim isn't ready and a pin has been provided, try to unlock the sim
+    if (ret != SIM_READY && pin != NULL && strlen(pin) > 0) {
+      simUnlock(pin);
+      return (getSimStatus() == SIM_READY);
+    } else {
+      // if the sim is ready, or it's locked but no pin has been provided,
+      // return true
+      return (ret == SIM_READY || ret == SIM_LOCKED);
+    }
+  }
+
+  /*
+   * Power functions
+   */
+ protected:
+  // Follows the A7670X template
+
+  /*
+   * Generic network functions
+   */
+ protected:
+
+  /*
+   * GPRS functions
+   */
+ protected:
+  bool gprsConnectImpl(const char* apn, const char* user = NULL,
+                       const char* pwd = NULL) {
+    gprsDisconnect();  // Make sure we're not connected first
+
+    // Define the PDP context
+
+    // The CGDCONT commands set up the "external" PDP context
+
+    // Set the external authentication
+    if (user && strlen(user) > 0) {
+      sendAT(GF("+CGAUTH=1,0,\""), user, GF("\",\""), pwd, '"');
+      waitResponse();
+    }
+
+    // Define external PDP context 1
+    sendAT(GF("+CGDCONT=1,\"IP\",\""), apn, '"', ",\"0.0.0.0\",0,0");
+    waitResponse();
+
+    // Configure TCP parameters
+
+    // Select TCP/IP application mode (command mode)
+    sendAT(GF("+CIPMODE=0"));
+    waitResponse();
+
+    // Set Sending Mode - send without waiting for peer TCP ACK
+    sendAT(GF("+CIPSENDMODE=0"));
+    waitResponse();
+
+    // Configure socket parameters
+    // AT+CIPCCFG= <NmRetry>, <DelayTm>, <Ack>, <errMode>, <HeaderType>,
+    //            <AsyncMode>, <TimeoutVal>
+    // NmRetry = number of retransmission to be made for an IP packet
+    //         = 10 (default)
+    // DelayTm = number of milliseconds to delay before outputting received data
+    //          = 0 (default)
+    // Ack = sets whether reporting a string "Send ok" = 0 (don't report)
+    // errMode = mode of reporting error result code = 0 (numberic values)
+    // HeaderType = which data header of receiving data in multi-client mode
+    //            = 1 (+RECEIVE,<link num>,<data length>)
+    // AsyncMode = sets mode of executing commands
+    //           = 0 (synchronous command executing)
+    // TimeoutVal = minimum retransmission timeout in milliseconds = 75000
+    sendAT(GF("+CIPCCFG=10,0,0,0,1,0,75000"));
+    if (waitResponse() != 1) { 
+      // printf("[%u]Configure socket parameters\n ",__LINE__);
+      return false; 
+    }
+
+    // Configure timeouts for opening and closing sockets
+    // AT+CIPTIMEOUT=<netopen_timeout> <cipopen_timeout>, <cipsend_timeout>
+    sendAT(GF("+CIPTIMEOUT="), 75000, ',', 15000, ',', 15000);
+    waitResponse();
+
+    // PDP context activate or deactivate
+    sendAT("+CGACT=1,1");
+    if (waitResponse(30000UL) != 1) { 
+      // printf("[%u]PDP context activate or deactivate\n ",__LINE__);
+      return false; 
+    }
+
+    // This activates and attaches to the external PDP context that is tied
+    // to the embedded context for TCP/IP (ie AT+CGACT=1,1 and AT+CGATT=1)
+    // Response may be an immediate "OK" followed later by "+NETOPEN: 0".
+    // We to ignore any immediate response and wait for the
+    // URC to show it's really connected.
+    sendAT(GF("+NETOPEN"));
+    if (waitResponse(75000L, GF(GSM_NL "+NETOPEN: 0")) != 1) { 
+      // printf("[%u]NETOPEN\n ",__LINE__);
+      return false; 
+    }
+
+    return true;
+  }
+
+  bool gprsDisconnectImpl() {
+    // Close all sockets and stop the socket service
+    // Note: On the LTE models, this single command closes all sockets and the
+    // service
+    sendAT(GF("+NETCLOSE"));
+    if (waitResponse(60000L, GF(GSM_NL "+NETCLOSE: 0")) != 1) { return false; }
+
+    return true;
+  }
+
+  bool isGprsConnectedImpl() {
+    sendAT(GF("+NETOPEN?"));
+    // May return +NETOPEN: 1, 0.  We just confirm that the first number is 1
+    if (waitResponse(GF(GSM_NL "+NETOPEN: 1")) != 1) { return false; }
+    waitResponse();
+    return true;
+  }
+
+  /*
+   * SIM card functions
+   */
+ protected:
+  // Follows the A7670X template
+
+  /*
+   * Messaging functions
+   */
+ protected:
+  // Follows all messaging functions per template
+
+  /*
+   * GPS/GNSS/GLONASS location functions
+   */
+ protected:
+  // Follows the A7670X template
+
+  /*
+   * Time functions
+   */
+  // Can follow CCLK as per template
+
+  /*
+   * NTP server functions
+   */
+  // Can sync with server using CNTP as per template
+
+  /*
+   * Battery functions
+   */
+ protected:
+  // Follows all battery functions per template
+
+  /*
+   * Client related functions
+   */
+ protected:
+  bool modemConnect(const char* host, uint16_t port, uint8_t mux,
+                    bool ssl = false, int timeout_s = 15) {
+    if (ssl) { DBG("SSL not yet supported on this module!"); }
+    // Make sure we'll be getting data manually on this connection
+    sendAT(GF("+CIPRXGET=1"));
+    if (waitResponse() != 1) { return false; }
+
+    // Establish a connection in multi-socket mode
+    uint32_t timeout_ms = ((uint32_t)timeout_s) * 1000;
+    sendAT(GF("+CIPOPEN="), mux, ',', GF("\"TCP"), GF("\",\""), host, GF("\","),
+           port);
+    // The reply is OK followed by +CIPOPEN: <link_num>,<err> where <link_num>
+    // is the mux number and <err> should be 0 if there's no error
+    if (waitResponse(timeout_ms, GF(GSM_NL "+CIPOPEN:")) != 1) { return false; }
+    uint8_t opened_mux    = streamGetIntBefore(',');
+    uint8_t opened_result = streamGetIntBefore('\n');
+
+    // printf("\nopened_mux :%u opened_result:%u\n",opened_mux,opened_result);
+    // switch (opened_result)
+    // {
+    //   case 0  : printf("operation succeeded\n");break;
+    //   case 1  : printf("Network failure\n");break;
+    //   case 2  : printf("Network not opened\n");break;
+    //   case 3  : printf("Wrong parameter\n");break;
+    //   case 4  : printf("Operation not supported\n");break;
+    //   case 5  : printf("Failed to create socket\n");break;
+    //   case 6  : printf("Failed to bind socket\n");break;
+    //   case 7  : printf("TCP server is already listening\n");break;
+    //   case 8  : printf("Busy\n");break;
+    //   case 9  : printf("Sockets opened\n");break;
+    //   case 10 : printf(" Timeout\n");break;
+    //   case 11 : printf(" DNS parse failed for AT+CIPOPEN\n");break;
+    //   case 12 : printf(" Unknown error\n");break;
+    // default:
+    //   break;
+    // }
+    if (opened_mux != mux || opened_result != 0) return false;
+    return true;
+  }
+
+    int16_t modemSend(const void* buff, size_t len, uint8_t mux) {
+    sendAT(GF("+CIPSEND="), mux, ',', (uint16_t)len);
+    if (waitResponse(GF(">")) != 1) { return 0; }
+    stream.write(reinterpret_cast<const uint8_t*>(buff), len);
+    stream.flush();
+    if (waitResponse(GF(GSM_NL "+CIPSEND:")) != 1) { return 0; }
+    streamSkipUntil(',');  // Skip mux
+    streamSkipUntil(',');  // Skip requested bytes to send
+    // TODO(?):  make sure requested and confirmed bytes match
+    return streamGetIntBefore('\n');
+  }
+
+  size_t modemRead(size_t size, uint8_t mux) {
+    if (!sockets[mux]) return 0;
+#ifdef TINY_GSM_USE_HEX
+    sendAT(GF("+CIPRXGET=3,"), mux, ',', (uint16_t)size);
+    if (waitResponse(GF("+CIPRXGET:")) != 1) { return 0; }
+#else
+    sendAT(GF("+CIPRXGET=2,"), mux, ',', (uint16_t)size);
+    if (waitResponse(GF("+CIPRXGET:")) != 1) { return 0; }
+#endif
+    streamSkipUntil(',');  // Skip Rx mode 2/normal or 3/HEX
+    streamSkipUntil(',');  // Skip mux/cid (connecion id)
+    int16_t len_requested = streamGetIntBefore(',');
+    //  ^^ Requested number of data bytes (1-1460 bytes)to be read
+    int16_t len_confirmed = streamGetIntBefore('\n');
+    // ^^ The data length which not read in the buffer
+    for (int i = 0; i < len_requested; i++) {
+      uint32_t startMillis = millis();
+#ifdef TINY_GSM_USE_HEX
+      while (stream.available() < 2 &&
+             (millis() - startMillis < sockets[mux]->_timeout)) {
+        TINY_GSM_YIELD();
+      }
+      char buf[4] = {
+          0,
+      };
+      buf[0] = stream.read();
+      buf[1] = stream.read();
+      char c = strtol(buf, NULL, 16);
+#else
+      while (!stream.available() &&
+             (millis() - startMillis < sockets[mux]->_timeout)) {
+        TINY_GSM_YIELD();
+      }
+      char c = stream.read();
+#endif
+      sockets[mux]->rx.put(c);
+    }
+    // DBG("### READ:", len_requested, "from", mux);
+    // sockets[mux]->sock_available = modemGetAvailable(mux);
+    sockets[mux]->sock_available = len_confirmed;
+    waitResponse();
+    return len_requested;
+  }
+
+  size_t modemGetAvailable(uint8_t mux) {
+    if (!sockets[mux]) return 0;
+    sendAT(GF("+CIPRXGET=4,"), mux);
+    size_t result = 0;
+    if (waitResponse(GF("+CIPRXGET:")) == 1) {
+      streamSkipUntil(',');  // Skip mode 4
+      streamSkipUntil(',');  // Skip mux
+      result = streamGetIntBefore('\n');
+      waitResponse();
+    }
+    // DBG("### Available:", result, "on", mux);
+    if (!result) { sockets[mux]->sock_connected = modemGetConnected(mux); }
+    return result;
+  }
+  
+  bool modemGetConnected(uint8_t mux) {
+    // Read the status of all sockets at once
+    sendAT(GF("+CIPCLOSE?"));
+    if (waitResponse(GF("+CIPCLOSE:")) != 1) {
+      // return false;  // TODO:  Why does this not read correctly?
+    }
+    for (int muxNo = 0; muxNo < TINY_GSM_MUX_COUNT; muxNo++) {
+      // +CIPCLOSE:<link0_state>,<link1_state>,...,<link9_state>
+      bool muxState = stream.parseInt();
+      if (sockets[muxNo]) { sockets[muxNo]->sock_connected = muxState; }
+    }
+    waitResponse();  // Should be an OK at the end
+    if (!sockets[mux]) return false;
+    return sockets[mux]->sock_connected;
+  }
+
+  /*
+   * Utilities
+   */
+ public:
+  // TODO(vshymanskyy): Optimize this!
+  int8_t waitResponse(uint32_t timeout_ms, String& data,
+                      GsmConstStr r1 = GFP(GSM_OK),
+                      GsmConstStr r2 = GFP(GSM_ERROR),
+#if defined TINY_GSM_DEBUG
+                      GsmConstStr r3 = GFP(GSM_CME_ERROR),
+                      GsmConstStr r4 = GFP(GSM_CMS_ERROR),
+#else
+                      GsmConstStr r3 = NULL, GsmConstStr r4 = NULL,
+#endif
+                      GsmConstStr r5 = NULL) {
+    /*String r1s(r1); r1s.trim();
+    String r2s(r2); r2s.trim();
+    String r3s(r3); r3s.trim();
+    String r4s(r4); r4s.trim();
+    String r5s(r5); r5s.trim();
+    DBG("### ..:", r1s, ",", r2s, ",", r3s, ",", r4s, ",", r5s);*/
+    data.reserve(64);
+    uint8_t  index       = 0;
+    uint32_t startMillis = millis();
+    do {
+      TINY_GSM_YIELD();
+      while (stream.available() > 0) {
+        TINY_GSM_YIELD();
+        int8_t a = stream.read();
+        // putchar(a);
+        if (a <= 0) continue;  // Skip 0x00 bytes, just in case
+        data += static_cast<char>(a);
+        if (r1 && data.endsWith(r1)) {
+          index = 1;
+          goto finish;
+        } else if (r2 && data.endsWith(r2)) {
+          index = 2;
+          goto finish;
+        } else if (r3 && data.endsWith(r3)) {
+#if defined TINY_GSM_DEBUG
+          if (r3 == GFP(GSM_CME_ERROR)) {
+            streamSkipUntil('\n');  // Read out the error
+          }
+#endif
+          index = 3;
+          goto finish;
+        } else if (r4 && data.endsWith(r4)) {
+          index = 4;
+          goto finish;
+        } else if (r5 && data.endsWith(r5)) {
+          index = 5;
+          goto finish;
+        } else if (data.endsWith(GF(GSM_NL "+CIPRXGET:"))) {
+          int8_t mode = streamGetIntBefore(',');
+          if (mode == 1) {
+            int8_t mux = streamGetIntBefore('\n');
+            if (mux >= 0 && mux < TINY_GSM_MUX_COUNT && sockets[mux]) {
+              sockets[mux]->got_data = true;
+            }
+            data = "";
+            // DBG("### Got Data:", mux);
+          } else {
+            data += mode;
+          }
+        } else if (data.endsWith(GF(GSM_NL "+RECEIVE:"))) {
+          int8_t  mux = streamGetIntBefore(',');
+          int16_t len = streamGetIntBefore('\n');
+          if (mux >= 0 && mux < TINY_GSM_MUX_COUNT && sockets[mux]) {
+            sockets[mux]->got_data = true;
+            if (len >= 0 && len <= 1024) { sockets[mux]->sock_available = len; }
+          }
+          data = "";
+          // DBG("### Got Data:", len, "on", mux);
+        } else if (data.endsWith(GF("+IPCLOSE:"))) {
+          int8_t mux = streamGetIntBefore(',');
+          streamSkipUntil('\n');  // Skip the reason code
+          if (mux >= 0 && mux < TINY_GSM_MUX_COUNT && sockets[mux]) {
+            sockets[mux]->sock_connected = false;
+          }
+          data = "";
+          DBG("### Closed: ", mux);
+        } else if (data.endsWith(GF("+CIPEVENT:"))) {
+          // Need to close all open sockets and release the network library.
+          // User will then need to reconnect.
+          DBG("### Network error!");
+          if (!isGprsConnected()) { gprsDisconnect(); }
+          data = "";
+        }
+      }
+    } while (millis() - startMillis < timeout_ms);
+  finish:
+    if (!index) {
+      data.trim();
+      if (data.length()) { DBG("### Unhandled:", data); }
+      data = "";
+    }
+    // data.replace(GSM_NL, "/");
+    // DBG('<', index, '>', data);
+    return index;
+  }
+
+  int8_t waitResponse(uint32_t timeout_ms, GsmConstStr r1 = GFP(GSM_OK),
+                      GsmConstStr r2 = GFP(GSM_ERROR),
+#if defined TINY_GSM_DEBUG
+                      GsmConstStr r3 = GFP(GSM_CME_ERROR),
+                      GsmConstStr r4 = GFP(GSM_CMS_ERROR),
+#else
+                      GsmConstStr r3 = NULL, GsmConstStr r4 = NULL,
+#endif
+                      GsmConstStr r5 = NULL) {
+    String data;
+    return waitResponse(timeout_ms, data, r1, r2, r3, r4, r5);
+  }
+
+  int8_t waitResponse(GsmConstStr r1 = GFP(GSM_OK),
+                      GsmConstStr r2 = GFP(GSM_ERROR),
+#if defined TINY_GSM_DEBUG
+                      GsmConstStr r3 = GFP(GSM_CME_ERROR),
+                      GsmConstStr r4 = GFP(GSM_CMS_ERROR),
+#else
+                      GsmConstStr r3 = NULL, GsmConstStr r4 = NULL,
+#endif
+                      GsmConstStr r5 = NULL) {
+    return waitResponse(1000, r1, r2, r3, r4, r5);
+  }
+
+
+ protected:
+  GsmClientA7670* sockets[TINY_GSM_MUX_COUNT];
+};
+
+#endif  // SRC_TINYGSMCLIENTA7670_H_
